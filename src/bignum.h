c5383526994352bb40ddd4494ba0f5c4f21f5160
MNIST <c782464295@163.com>
2018-08-30 20:20:06 +0800

    Update bignum.h

--- src/bignum.h
+++ src/bignum.h
@@ -1,6 +1,4 @@
-// Copyright (c) 2009 Satoshi Nakamoto
-// Distributed under the MIT/X11 software license, see the accompanying
-// file license.txt or http://www.opensource.org/licenses/mit-license.php.
+
 
 #include <stdexcept>
 #include <vector>
