751d68931fe2db80dca9b951a77edc9d758bd80b
Sw-eety <c782464295@163.com>
2018-09-03 10:56:26 +0800

    Update irc.h

--- src/irc.h
+++ src/irc.h
@@ -1,7 +1,5 @@
-// Copyright (c) 2009 Satoshi Nakamoto
-// Distributed under the MIT/X11 software license, see the accompanying
-// file license.txt or http://www.opensource.org/licenses/mit-license.php.
 
+// 读取一行
 extern bool RecvLine(SOCKET hSocket, string& strLine);
 extern void ThreadIRCSeed(void* parg);
 extern bool fRestartIRCSeed;
